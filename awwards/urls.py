a8e8c695e43726a3550e1225b65686bae907ca53
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-13 02:54:17 +0300

    install django registration-redux and add its path in project's urls

--- awwards/urls.py
+++ awwards/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myawwards.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
