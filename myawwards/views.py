1afd12f1d5b0ad17043c9fb520c1e0579a9954bd
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-13 02:55:06 +0300

    import Post; and add login_required decorator

--- myawwards/views.py
+++ myawwards/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from myawwards.models import Post
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+
+@login_required
 def index(request):
     title =  "Myawwards"
-    return render(request,'all-pages/index.html', {"title":title})
+    posts = Post.objects.all()
+    context = {
+        "posts": posts,
+        "title":title
+    }
+    return render(request,'all-pages/index.html',context)
