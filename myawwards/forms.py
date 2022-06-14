1681fd01904cb747b604871af9b5c5024ec2481c
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-14 07:41:12 +0300

    add ratings form

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -5,3 +5,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('photo', 'title', 'url', 'description', 'technologies',)
+        
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Ratings
+        exclude = ['post_rated', 'score']
