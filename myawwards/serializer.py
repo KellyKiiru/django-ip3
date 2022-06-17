eed8462405e389a4a1ffe79c25cdd82f4acf7a0c
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-17 12:31:54 +0300

    create serializer file with its url

---
+++ myawwards/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from rest_framework import status
+from .models import *
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+      model=Profile
+      fields=('profile_picture','bio','email')
