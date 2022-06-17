54a932439e82734de66cba4a086fdb64e221eb6d
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-17 09:35:36 +0300

    change database

--- awwards/settings.py
+++ awwards/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myawwards',
+        'NAME': 'myawwardss',
         'USER': 'moringa',
         'PASSWORD': 'Access',
     }
@@ -143,6 +143,8 @@ CRISPY_CLASS_CONVERTERS = {
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
