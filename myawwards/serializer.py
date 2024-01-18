13255cce299519e8faecc053c68afc239a41411f
kellykiiru <infowithkiiru@gmail.com>
2024-01-18 23:16:05 +0300

    uncomment serializer code

--- myawwards/serializer.py
+++ myawwards/serializer.py
@@ -1,16 +1,16 @@
-#from rest_framework import serializers
-#from rest_framework.response import Response
-#from rest_framework.views import APIView
-#from .serializer import *
-#from rest_framework import status
-#from .models import *
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from rest_framework import status
+from .models import *
 
-#class ProfileSerializer(serializers.ModelSerializer):
-#    class Meta:
-#      model=Profile
-#      fields=('profile_picture','bio','email')
+class ProfileSerializer(serializers.ModelSerializer):
+   class Meta:
+     model=Profile
+     fields=('profile_picture','bio','email')
 
-#class PostSerializer(serializers.ModelSerializer):
-#    class Meta:
-#      model=Post
-#      fields=('id','title', 'photo', 'description', 'url','profile','user')
+class PostSerializer(serializers.ModelSerializer):
+   class Meta:
+     model=Post
+     fields=('id','title', 'photo', 'description', 'url','profile','user')
